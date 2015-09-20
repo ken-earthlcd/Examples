9c886e762c0f8f66854f6dd88db6e913dcaf0ac5
pyOpy <ken@earthlcd.com>
2015-09-19 17:54:41 -0700

    Update LedFlasher.py

--- LedFlasher.py
+++ LedFlasher.py
@@ -1,8 +1,7 @@
 # Simple led flasher for pyopy-mini
 led = pyb.LED(1)
 while True:
-	led.on()
-	pyb.delay(500)
-	led.off()
-	pyb.delay(500)
-  
+    led.on()
+    pyb.delay(500)
+    led.off()
+    pyb.delay(500)
